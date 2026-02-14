0cecf860ac0dde339d233740a280330309aa83cb
Marko Lekic <mirrorsandmisdirections@gmail.com>
2026-02-14 15:52:39 +0100

    V1 T10.1

---
+++ crates/distributed/build.rs
@@ -0,0 +1,16 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo:rerun-if-changed=proto/ffq_distributed.proto");
+
+    if std::env::var_os("CARGO_FEATURE_GRPC").is_none() {
+        return Ok(());
+    }
+
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+    std::env::set_var("PROTOC", protoc);
+
+    tonic_build::configure().compile_protos(
+        &["proto/ffq_distributed.proto"],
+        &["proto"],
+    )?;
+    Ok(())
+}
--- crates/distributed/src/grpc.rs
+++ crates/distributed/src/grpc.rs
@@ -1,1 +1,10 @@
-// v1 skeleton: tonic service definitions + generated protos will be added later.
+pub mod v1 {
+    tonic::include_proto!("ffq.distributed.v1");
+}
+
+pub use v1::control_plane_client::ControlPlaneClient;
+pub use v1::control_plane_server::{ControlPlane, ControlPlaneServer};
+pub use v1::heartbeat_service_client::HeartbeatServiceClient;
+pub use v1::heartbeat_service_server::{HeartbeatService, HeartbeatServiceServer};
+pub use v1::shuffle_service_client::ShuffleServiceClient;
+pub use v1::shuffle_service_server::{ShuffleService, ShuffleServiceServer};
